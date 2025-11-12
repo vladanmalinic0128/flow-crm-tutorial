aeae6d338ebf7c003cef57a369cbc39add610667
Vladan Malinic <72340624+vladanmalinic0128@users.noreply.github.com>
2025-11-12 19:09:21 +0100

    Added new president file

--- src/main/java/com/example/application/initializers/PresidentInitializer.java
+++ src/main/java/com/example/application/initializers/PresidentInitializer.java
@@ -28,7 +28,8 @@ public class PresidentInitializer /*implements ApplicationRunner*/ {
 
     //@Override
     public void run(ApplicationArguments args) throws Exception {
-        String absolutePath = "src/main/resources/documents/predsjednici2025.xlsx";
+        //String absolutePath = "src/main/resources/documents/predsjednici2025.xlsx";
+        String absolutePath = "src/main/resources/documents/predsjednici2025_2.xlsx";
         try (InputStream inputStream = new FileInputStream(absolutePath)) {
             Workbook workbook = WorkbookFactory.create(inputStream);
             Sheet sheet = workbook.getSheetAt(0);
